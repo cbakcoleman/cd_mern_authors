15627db9bd70612111a5b33b7146b98c0cd2e7d5
cbakcoleman <88118758+cbakcoleman@users.noreply.github.com>
2021-11-10 19:15:15 -0800

    set up controller

---
+++ server/controllers/Author.controllers.js
@@ -0,0 +1,34 @@
+const Author = require("../models/Author.models");
+
+module.exports = {
+    // READ ALL
+    findAllAuthors : (req, res) => {
+        Author.find()
+            .then(allAuthors => res.json({ message: "Success!", authors: allAuthors }))
+            .catch(err => res.json({ message: "Pobody's Nerfect!", error: err}));
+    }, 
+    // READ ONE
+    fineOneAuthor : (req, res) => {
+        Author.findById(req.params.id)
+            .then(oneAuthor => res.json({ message: "Success!", author: oneAuthor}))
+            .catch(err => res.json({ message: "Pobody's Nerfect!", error: err}));
+    },
+    // CREATE
+    createNewAuthor : (req, res) => {
+        Author.create(req.body)
+            .then(newAuthor => res.json({ message: "Success!", author: newAuthor }))
+            .catch(err => res.json({ message: "Pobody's Nerfect!", error: err}));
+    },
+    // UPDATE
+    updateAuthor : (req, res) => {
+        Author.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+            .then(updatedAuthor => res.json({ message: "Success!", author: updatedAuthor }))
+            .catch(err => res.json({ message: "Pobody's Nerfect!", error: err }));
+    },
+    // DESTROYYYYY
+    deleteAuthor : (req, res) => {
+        Author.findByIdAndDelete(req.params.id)
+            .then(result => res.json({ message: "Success!", result: result }))
+            .catch(err => res.json({ message: "Pobody's Nerfect!", error: err }));
+    }
+}
